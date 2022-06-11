63b9a3550188c98aa5814d50c34407ade58fa7fd
Ouzinem <ouzinem@hotmail.com>
2022-06-11 16:20:41 -0400

    add pokemonList and function open

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
-  styles: [],
+  template: ` <h1>Welcome to {{ title }}!</h1>
+    <ul class="text-class">
+      <li *ngFor="let item of pokemonList" (click) ="open(item)">{{ item }}</li>
+    </ul>`,
+  styles: [
+    `.text-class {
+      color: red; 
+      list-style-type: decimal;}`
+  ],
 })
-export class AppComponent {
-  title = "ng-pokemon-app";
+export class AppComponent implements OnInit {
+  title: string = "Application Pokemon";
+  pokemonList: string[] = ["Pikachu", "Bulbasaur", "Charmander", "Squirtle"];
+  ngOnInit() {
+    console.table(this.pokemonList); 
+  }
+  open(pokemonName: string) {
+    alert(`You clicked on a pokemon ${pokemonName}`);
+  }
 }
