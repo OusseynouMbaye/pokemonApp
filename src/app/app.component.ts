28d3dc50ae997bbc64336b7c4f43d2f976933e75
Ouzinem <ouzinem@hotmail.com>
2022-06-24 17:02:56 -0400

    affichage  du nom du pokemon quand on clique sur le clavier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,40 +4,29 @@ import { Pokemon } from "./pokemon";
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
-  styles: [
-    `
-      .pokemon-grid {
-        color: red;
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-        gap: 10px;
-        justify-items: center;
-      }
-      .title-pokemon {
-        text-align: center;
-      }
-      .pokemon-img {
-        display: flex;
-        justify-content: center;
-      }
-      .pokemon-card {
-        background: beige;
-        min-width: 100%;
-      }
-      .pokemon-div{
-        
-      }
-    `,
-  ],
+  styleUrls: [`./app-component.css`],
 })
 export class AppComponent implements OnInit {
   title: string = "Pokemon List";
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
+
   ngOnInit() {
     console.table(this.pokemonList);
+    // this.pokemonSelected;
   }
-  selectPokemon(event: MouseEvent) {
-    const index: number = +(event.target as HTMLInputElement).value;
-    alert(`You clicked on a pokemon ${this.pokemonList[index].name}`);
+
+  selectPokemon(pokemonId: String) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+
+    if (pokemon) {
+      console.log(`Vous avez demande le pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Le pokemon demande n'existe pas`);
+      this.pokemonSelected = pokemon;
+    }
   }
 }
